72057635088e682ddc4de2eafd19e907edcba55f
liveforone <yc4852@gmail.com>
2024-03-29 15:22:55 +0900

    Remove users repository

--- nestjs-backend/src/users/users.module.ts
+++ nestjs-backend/src/users/users.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './service/users.service';
 import { UsersController } from './controller/users.controller';
-import { UsersRepository } from './repository/users.repository';
 import { RedisModule } from 'src/redis/redis.module';
 
 @Module({
   imports: [RedisModule],
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository],
+  providers: [UsersService],
   exports: [UsersService],
 })
 export class UsersModule {}
