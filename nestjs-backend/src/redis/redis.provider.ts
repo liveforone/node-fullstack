a6f2324bc47fb3d048283a47780037fa7145f9c5
liveforone <yc4852@gmail.com>
2024-03-29 16:59:45 +0900

    Refactor redis url change to docker

--- nestjs-backend/src/redis/redis.provider.ts
+++ nestjs-backend/src/redis/redis.provider.ts
@@ -7,8 +7,8 @@ export const redisProvider = [
     useFactory: async () => {
       const client = createClient({
         //아래 url은 개발 단계에서 사용된다.
-        url: 'redis://default:159624@localhost:6379',
-        // url: 'redis://default:159624@host.docker.internal:6379',
+        // url: 'redis://default:159624@localhost:6379',
+        url: 'redis://default:159624@host.docker.internal:6379',
       });
       await client.connect();
       return client;
