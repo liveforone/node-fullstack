f12627f1d4e53802ae7ee83827d51a3b0dc21c9f
liveforone <yc4852@gmail.com>
2024-03-29 15:23:25 +0900

    Refactor validate error msg

--- nestjs-backend/src/common/found-data.validator.ts
+++ nestjs-backend/src/common/found-data.validator.ts
@@ -3,7 +3,7 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 export const validateFoundData = (foundData: any): void => {
   if (!foundData) {
     throw new HttpException(
-      '데이터가 존재하지 않습니다.',
+      'Record Is Not Exist In Database',
       HttpStatus.NOT_FOUND,
     );
   }
